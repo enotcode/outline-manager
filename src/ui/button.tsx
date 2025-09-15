19cb666dc33066f073f9fcc9b208c8eb9a8f430d
Viktor Fomin <viktor.fomin@onlyoffice.com>
2025-09-16 00:10:55 +0300

    chore: use class variance authority

--- src/ui/button.tsx
+++ src/ui/button.tsx
@@ -1,17 +1,24 @@
 import React from "react";
+import { cva, type VariantProps } from "class-variance-authority";
+
+const buttonVariants = cva(
+  "w-full text-white font-bold py-2 px-4 rounded-lg cursor-pointer disabled:pointer-events-none disabled:opacity-50",
+  {
+    variants: {
+      variant: {
+        default: "bg-teal-700 hover:bg-teal-800",
+        danger: "bg-red-700 hover:bg-red-800",
+      },
+    },
+    defaultVariants: {
+      variant: "default",
+    },
+  }
+);
 
 export const Button = ({
   variant = "default",
   ...props
-}: React.ComponentProps<"button"> & { variant?: "default" | "danger" }) => {
-  const variantClass =
-    variant === "default"
-      ? "bg-teal-700 hover:bg-teal-800"
-      : "bg-red-700 hover:bg-red-800";
-  return (
-    <button
-      className={`w-full ${variantClass} text-white font-bold py-2 px-4 rounded-lg cursor-pointer`}
-      {...props}
-    />
-  );
+}: React.ComponentProps<"button"> & VariantProps<typeof buttonVariants>) => {
+  return <button className={buttonVariants({ variant })} {...props} />;
 };
